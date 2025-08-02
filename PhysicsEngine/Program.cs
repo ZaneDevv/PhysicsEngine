48d4d1325f4cb2594731f6d7645526455d07cb0a
ZaneDev <alvferbarrero@gmail.com>
2025-08-02 11:33:28 +0200

    Add project files.

---
+++ PhysicsEngine/Program.cs
@@ -0,0 +1,11 @@
+﻿using PhysicsEngine;
+using Microsoft.Xna.Framework;
+
+internal sealed class Program
+{
+    internal static void Main(string[] args)
+    {
+        Game Game = new World();
+        Game.Run();
+    }
+}
---
+++ PhysicsEngine/World.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysicsEngine
+{
+    public class World : Game
+    {
+        private readonly Color BACKGROUND_COLOR = new Color(21, 21, 21);
+
+        private GraphicsDeviceManager Graphics;
+
+        public World()
+        {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(BACKGROUND_COLOR);
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
