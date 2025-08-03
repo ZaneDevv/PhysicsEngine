0046c19c09d8608f5c4e2c26271c008c1f234e22
ZaneDev <alvferbarrero@gmail.com>
2025-08-03 22:53:29 +0200

    Collision logic comments added

--- PhysicsEngine/src/Collisions/Collision.cs
+++ PhysicsEngine/src/Collisions/Collision.cs
@@ -4,6 +4,16 @@ namespace PhysicsEngine.Collisions
 {
     internal struct Collision
     {
+        /// <summary>
+        /// Checks ahd gives information about the possible collision of two circles
+        /// </summary>
+        /// <param name="center1">Center position of the frist circle</param>
+        /// <param name="radius1">Radius of the first circle</param>
+        /// <param name="center2">Center position of the second circle</param>
+        /// <param name="radius2">Radius of the second circle</param>
+        /// <param name="normal">The normal of the collision</param>
+        /// <param name="depth">How much are the circles overlaping</param>
+        /// <returns>Returns if the two circles are overlaping</returns>
         internal static bool Circle_VS_Circle(Vector3 center1, double radius1, Vector3 center2, double radius2, out Vector3 normal, out double depth)
         {
             double radiusAddition = radius1 + radius2;
