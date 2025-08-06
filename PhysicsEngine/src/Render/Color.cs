2ac4c825035e6d195c324c68596b46090ec3b0ce
ZaneDev <alvferbarrero@gmail.com>
2025-08-06 17:20:23 +0200

    Refactored

--- PhysicsEngine/src/Render/Color.cs
+++ PhysicsEngine/src/Render/Color.cs
@@ -5,37 +5,58 @@ namespace PhysicsEngine.Render
 {
     internal struct ColorUtils
     {
+        /// <summary>
+        /// Turns a HSV color into a RGB color
+        /// </summary>
+        /// <param name="h">Hue</param>
+        /// <param name="s">Saturation</param>
+        /// <param name="v">Value</param>
+        /// <returns></returns>
         internal static Color HSVToRGB(float h, float s, float v)
         {
             float c = v * s;
             float x = c * (1 - Math.Abs((h / 60f) % 2 - 1));
             float m = v - c;
 
-            float r1 = 0, g1 = 0, b1 = 0;
+            float r1 = 0;
+            float g1 = 0;
+            float b1 = 0;
 
             if (h < 60)
             {
-                r1 = c; g1 = x; b1 = 0;
+                r1 = c;
+                g1 = x;
+                b1 = 0;
             }
             else if (h < 120)
             {
-                r1 = x; g1 = c; b1 = 0;
+                r1 = x;
+                g1 = c;
+                b1 = 0;
             }
             else if (h < 180)
             {
-                r1 = 0; g1 = c; b1 = x;
+                r1 = 0;
+                g1 = c;
+                b1 = x;
             }
             else if (h < 240)
             {
-                r1 = 0; g1 = x; b1 = c;
+                r1 = 0;
+                g1 = x;
+                b1 = c;
             }
             else if (h < 300)
             {
-                r1 = x; g1 = 0; b1 = c;
+                r1 = x;
+                g1 = 0; 
+                b1 = c;
             }
             else
             {
-                r1 = c; g1 = 0; b1 = x;
+                r1 = c;
+                g1 = 0;
+                b1 = x;
             }
 
             byte r = (byte)(MathHelper.Clamp((r1 + m) * 255, 0, 255));
